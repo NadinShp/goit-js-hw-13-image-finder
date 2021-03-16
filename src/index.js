e999a6fd0d264d43d6b801d764533dc9413c1d4a
NadinShp <70705043+NadinShp@users.noreply.github.com>
2021-03-16 18:17:18 +0200

    добавлено нотификации по результату fetch

--- src/index.js
+++ src/index.js
@@ -2,30 +2,39 @@ import './styles.css';
 import PhotoPagination from './photoPagination/index';
 import photoCard from './templates/photoCard.hbs';
 import createMurkupHtml from './js/murkup-html';
+import { alert, error, success, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+// import originImgCard from './templates/originImgCard.hbs';
+const { defaults } = require('@pnotify/core');
+defaults.styling = 'material';
+defaults.icons = 'material';
 
 createMurkupHtml();
 
 const formRef = document.querySelector(".search-form");
 const buttonRef = document.querySelector(".btn");
 const galleryRef = document.querySelector(".gallery");
+// const backdropRef = document.querySelector(".backdrop");
 
 const photoPagination = new PhotoPagination();
 
 formRef.addEventListener('submit', onSearch);
 buttonRef.addEventListener('click', loadMore);
+// galleryRef.addEventListener('click', clickImageHandler);
+
 let id = 0;
 let height;
+// const arrayPhoto = [];
 
 function onSearch(event) {
     event.preventDefault();
     photoPagination.query = event.currentTarget.elements.query.value;
     photoPagination.resetPage();
-    height = '';
     clearTimeout(id);
-    // console.log(height);
     photoPagination.fetchPhotos().then(data => {
         cleanContainer();
         appendPhotosMurkup(data);
+        // createaArrayAllPhoto(data);
     });
 }
 async function loadMore() {
@@ -48,3 +57,22 @@ function appendPhotosMurkup(array) {
 function cleanContainer() {
     galleryRef.innerHTML = ' ';
 }
+
+
+// function clickImageHandler(event) {
+//     event.preventDefault();
+//     const index = event.target.attributes.index.nodeValue;
+//     // console.dir(a);
+//     if (event.target.nodeName !== "IMG") return;
+//     backdropRef.classList.add("is-open");
+//     console.log(arrayPhoto);
+// }
+// function createaArrayAllPhoto(data) {
+//      const murkupPhoto = data.map(element =>photoAdapterArray(element));
+//     console.log(murkupPhoto);
+//     return murkupPhoto;
+// }
+// const photoAdapterArray = ({ id, largeImageURL }) => ({
+//     src: `${largeImageURL}`,
+//     id: `${id}`,
+// });
--- src/photoPagination/index.js
+++ src/photoPagination/index.js
@@ -1,20 +1,47 @@
 import apiService from '../js/apiService';
+import { alert, error, success, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import {Stack} from '@pnotify/core';
+const { defaults } = require('@pnotify/core');
+defaults.styling = 'material';
+defaults.icons = 'material';
 
 export default class PhotoPagination {
+    #arrayPhotos = [];
     constructor() {
         this.searchPhoto = ' ';
         this.page = 1;
         this.total_page = '';
+        // this.array = [];
     }
     fetchPhotos() {
-        // console.log(this);
+        console.log(this);
         return apiService.requestPath(this.searchPhoto, this.page)
             .then(data => {
                 this.total_page = data.totalHits;
                 this.incrementPage();
 
+                  defaultModules.set(PNotifyMobile, {});
+                  success({
+                      text: 'Images loaded success!',
+                stack: new Stack({
+                dir1: 'up', dir2: 'left',
+                    firstpos1: 25, firstpos2: 25
+                  })
+                });
+
                 return data.hits;
-            }).catch(error => console.log('ERROR', error));
+            }).catch(error => {
+                console.log('ERROR', error);
+                defaultModules.set(PNotifyMobile, {});
+                error({
+                text: 'Error loading images!',
+                stack: new Stack({
+                dir1: 'up', dir2: 'left', // Position from the top left corner.
+                firstpos1: 25, firstpos2: 25 // 90px from the top, 90px from the left.
+  })
+});
+            });
     }
     get query() {
         return this.searchPhoto;
@@ -35,5 +62,10 @@ export default class PhotoPagination {
     resetPage() {
         this.page = 1;
     }
+    // get arrayPhoto() {
+    //     return this.array;
+    // }
+    // set arrayPhoto(data) {
+    //     this.arrayPhoto.push(data);
+    // }
 }
-
